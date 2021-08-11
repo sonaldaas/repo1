271d181d07661e87c0166a0b58d4b149fd29c206
sonaldaas <88396492+sonaldaas@users.noreply.github.com>
2021-08-11 06:14:38 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,15 +1,14 @@
 <?php
-/* Attempt MySQL server connection. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+
 $link = mysqli_connect("localhost:3307", "root", "", "demo");
  
-// Check connection
+
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 $id = mysqli_real_escape_string($link, $_POST['id']);
  
-// Attempt delete query execution
+
 $sql = "DELETE FROM persons WHERE id='$id'";
 if(mysqli_query($link, $sql)){
     echo "Records were deleted successfully.";
@@ -17,6 +16,6 @@ if(mysqli_query($link, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
-// Close connection
+
 mysqli_close($link);
 ?>
